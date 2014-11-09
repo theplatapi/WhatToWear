89f63351b6b293ee627a33a15437d46ad8665f44
Allen Korenevsky <theplatapi@gmail.com>
2014-11-09 15:20:39 -0800

    The time selector is now set to an initial wind

--- w2w.js
+++ w2w.js
@@ -1,8 +1,16 @@
 if (Meteor.isClient) {
   var last = 0;
   var currentRotation = 0;
+  var time = new ReactiveVar(moment());
 
   Template.timeSelector.rendered = function() {
+    //set initial wind. It is at 7am currently.
+    //calculate minutes difference
+    var minutesOff = time.get().diff(moment().hour(7).minute(0), 'minutes');
+
+    currentRotation = minutesOff / 4;
+    $('#timeSelector').css('transform', 'rotate(' + currentRotation + 'deg)');
+
     //set up event listener
     $('#scrollBox').scroll(function() {
       var newScroll = $('#scrollBox').scrollTop();
@@ -11,11 +19,16 @@ if (Meteor.isClient) {
       last = newScroll;
       currentRotation += scrollChange;
       $('#timeSelector').css('transform', 'rotate(' + currentRotation + 'deg)');
-      //set time of day
+      //4 minutes per degree
+      time.set(time.get().add(scrollChange * 4, 'minutes'));
     });
-
-    //TODO: Set to time of day currently
   };
+
+  Template.profiles.helpers({
+    getTime: function() {
+      return time.get().calendar();
+    }
+  });
 
   Template.avatar.helpers({
     getShirt: function() {
