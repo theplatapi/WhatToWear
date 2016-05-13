5c3ccd82fca0dd0b8f1270245fc9c73205799ca6
Allen Korenevsky <theplatapi@gmail.com>
2016-05-12 19:11:21 -0700

    More ES6 changes

--- client/timeSelector/timeSelector.js
+++ client/timeSelector/timeSelector.js
@@ -1,10 +1,9 @@
 import Big from 'big.js';
 import moment from 'moment';
-import { Template } from 'meteor/templating'
+import {Template} from 'meteor/templating'
 
-var currentRotation = 0;
-var last = 0;
-
+let currentRotation = 0;
+let last = 0;
 
 //update time every minute on the dot
 let interval = 60 * 1000;
@@ -15,51 +14,50 @@ Meteor.setInterval(function () {
 }, interval);
 
 
-var setScrollHeight = function (self, radius) {
+let setScrollHeight = function (template, radius) {
   //So we can at least scroll a bit.
   if (radius === 0) {
     radius = 20;
   }
   //circumference is 1 day
-  var circumference = new Big(Math.PI).times(2).times(radius);
-  var extraHeight = circumference.times(3);
-  var newHeight = $(document).height() + Math.round(extraHeight);
+  let circumference = new Big(Math.PI).times(2).times(radius);
+  let extraHeight = circumference.times(3);
+  let newHeight = $(document).height() + Math.round(extraHeight);
 
-  self.$('#scrollContent').css('height', newHeight + 'px');
+  template.$('#scrollContent').css('height', newHeight + 'px');
 };
 
 Template.timeSelector.rendered = function () {
-  var self = this;
+  let template = this;
   //set initial dial turn. Morning is 7am.
   let time = moment(Session.get('time'));
-  console.log("time", time.calendar());
-  var minutesOff = time.diff(moment().hour(7).minute(0), 'minutes');
-  var radius = self.$('#timeSelector').width() / 2;
+  let minutesOff = time.diff(moment().hour(7).minute(0), 'minutes');
+  let radius = template.$('#timeSelector').width() / 2;
 
   currentRotation = new Big(minutesOff / 4);
-  self.$('#timeSelector').css('transform', 'rotate(' + currentRotation + 'deg)');
+  template.$('#timeSelector').css('transform', 'rotate(' + currentRotation + 'deg)');
   //set initial scroll height
-  setScrollHeight(self, radius);
+  setScrollHeight(template, radius);
 
   //set up event listener
-  self.$('#scrollBox').scroll(function () {
-    var newRadius = self.$('#timeSelector').width() / 2;
+  template.$('#scrollBox').scroll(() => {
+    let newRadius = template.$('#timeSelector').width() / 2;
 
     //dial radius changed from screen resize.
     if (newRadius !== radius) {
-      setScrollHeight(self, newRadius);
+      setScrollHeight(template, newRadius);
       radius = newRadius;
     }
 
     //calculate degrees to move time dial
-    var newScroll = self.$('#scrollBox').scrollTop();
-    var scrollChange = newScroll - last;
-    var radians = new Big(Math.atan2(1, radius)).times(scrollChange);
-    var newRotation = radians.times(new Big(180).div(Math.PI));
+    let newScroll = template.$('#scrollBox').scrollTop();
+    let scrollChange = newScroll - last;
+    let radians = new Big(Math.atan2(1, radius)).times(scrollChange);
+    let newRotation = radians.times(new Big(180).div(Math.PI));
 
     last = newScroll;
     currentRotation = currentRotation.plus(newRotation);
-    self.$('#timeSelector').css('transform', 'rotate(' + currentRotation.toString() + 'deg)');
+    template.$('#timeSelector').css('transform', 'rotate(' + currentRotation.toString() + 'deg)');
     //4 minutes per degree
     Session.set('time', moment(Session.get('time')).add(newRotation.times(4).toString(), 'minutes').valueOf());
   });
